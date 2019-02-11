3d36f2ccb3c1df6f50aaa75bac611ddf7e1ef72c
Nazaled <40932170+Nazaled@users.noreply.github.com>
2019-02-11 17:46:23 +0100

    Create liveconfig-installer.sh

---
+++ liveconfig-installer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+clear
+read -p"Soll LiveConfig installiert werden? (j/n)? " response
+
+if [ "$response" == "j" ]; then
+# Ändert die hostname und host Datei
+        	clear
+        echo "Bitte geben Sie die Subdomain ein (z.B. www)."
+        	read subdomain
+        echo "Bitte geben Sie den Domainnamen ein."
+        	read domain
+        echo "Bitte geben Sie die IP Adresse des Servers ein."
+        	read ip
+
+        sudo printf "$subdomain">/etc/hostname
+        sudo printf "127.0.0.1  localhost\n 127.0.1.1   $subdomain.$domain $subdomain\n ::1             localhost ip6-localhost ip6-loopback\n ff02::1  ip6-allnodes\n ff02::2  ip6-allrouters\n $ip    $subdomain.$domain $subdomain">/etc/$
+        hostname -F /etc/hostname
+        clear
+
+        echo "Es wird ein Update und Upgrade durchgeführt"
+                sleep 2
+                sudo apt update && sudo apt upgrade -y
+                clear
+        echo "Paket ntp wird installiert"
+                sleep 2
+                sudo apt install ntp -y
+                clear
+        echo "Paket quota wird installiert"
+                sleep 2
+                sudo apt install quota -y
+                clear
+        echo "LiveConfig wird installiert"
+                sleep 2
+                wget -O - https://www.liveconfig.com/liveconfig.key | sudo apt-key add -
+                cd /etc/apt/sources.list.d
+                sudo wget http://repo.liveconfig.com/debian/liveconfig.list
+                sudo apt install aptitude -y && sudo aptitude update -y && sudo aptitude install liveconfig-meta -y && sudo aptitude install liveconfig -y
+# Fehlende Pakete installieren die für ein Webserver notwendig sind
+                sudo apt install php-fpm -y
+                sudo apt-get install spamassassin -y
+                aptitude install php-pear -y
+                sudo apt install apache2-suexec-custom -y
+                sudo systemctl restart apache2
+                clear
+
+        echo "Installation ist abgeschlossen Server wird jetzt neugestartet...."
+	sleep 2
+	sudo reboot now
+fi
